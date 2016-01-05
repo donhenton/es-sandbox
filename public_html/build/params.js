177a5bc9b0bc36f7eaf2bdea91452458089bdeb7
d <dhenton@localhost.com>
2016-01-05 12:07:22 -0600

    done with params

--- public_html/build/params.js
+++ public_html/build/params.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,12 +9,107 @@
  */
 
 function defaultDemo(anchor) {
-  var alpha = arguments.length <= 1 || arguments[1] === undefined ? 'get a job' : arguments[1];
+    var alpha = arguments.length <= 1 || arguments[1] === undefined ? 'get a job' : arguments[1];
 
-  $(anchor).text(alpha);
+    $(anchor).text(alpha);
 }
 
 //should do something
 defaultDemo('#example1');
 defaultDemo('#example2', 'get another job,baby!!!');
+
+var itemSample = ['bonzo', "dog", "band"];
+
+allSorts('#allList', "Snort", itemSample[0], itemSample[1], itemSample[2]);
+
+function allSorts(anchor, title) {
+    $('#allTitle').html(title);
+
+    for (var _len = arguments.length, items = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+        items[_key - 2] = arguments[_key];
+    }
+
+    $.each(items, function (idx, d) {
+
+        $(anchor).append('<li>' + d + '</li>');
+    });
+}
+
+$('#writeSomething').html(writeSomething('hello there ', 'John', 'Smith'));
+
+function writeSomething(message) {
+
+    return message + (arguments.length <= 2 ? undefined : arguments[2]) + ", " + (arguments.length <= 1 ? undefined : arguments[1]);
+}
+
+$('#destrucParam1').html(destructParam('Message', { 'last': 'Smith', 'first': 'John' }));
+
+function destructParam(msg, _ref) {
+    var lastName = _ref.last;
+    var firstName = _ref.first;
+
+    return msg + " ho ho " + lastName + ", " + firstName;
+}
+
+/////////////////////////////////////////////////
+
+var numbers = [10, 20, 30, 40, 50];
+
+$('#destrucArray1').append(destructArray(numbers));
+
+function destructArray(_ref2) {
+    var _ref3 = _toArray(_ref2);
+
+    var first = _ref3[0];
+    var second = _ref3[1];
+
+    var remainder = _ref3.slice(2);
+
+    var val = "<ol>";
+    val += "<li>" + first + '</li>';
+    val += "<li>" + second + '</li>';
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = remainder[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var item = _step.value;
+
+            val += "<li style='color:blue'>" + item + ' from for loop</li>';
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    remainder.forEach(function (item) {
+        val += "<li style='color:red'>" + item + '</li>';
+    });
+
+    val += "<li>rest has length of " + remainder.length + '</li>';
+
+    val = val + "</ol>";
+    return val;
+}
+
+var sample = { 'last': 'Smith', 'first': 'John' };
+$('#templateItem').append(templateDemo(sample));
+
+function templateDemo(thing) {
+    //requires backtick to get the interpolation
+    var templateString = '<h3>This is for ' + thing.first + ' ' + thing.last + '</h3>';
+    return templateString;
+}
 //# sourceMappingURL=params.js.map
--- src/params.js
+++ src/params.js
@@ -15,3 +15,80 @@ function defaultDemo(anchor,alpha='get a job')
 //should do something
 defaultDemo('#example1');
 defaultDemo('#example2','get another job,baby!!!');
+
+
+let itemSample= ['bonzo',"dog","band"];
+
+allSorts('#allList',"Snort",itemSample[0],itemSample[1],itemSample[2]);
+
+function allSorts(anchor,title,...items)
+{
+    $('#allTitle').html(title);
+    $.each(items,function(idx,d){
+        
+        $(anchor).append('<li>'+d+'</li>');
+    });
+    
+}
+
+
+
+$('#writeSomething').html(writeSomething('hello there ','John','Smith'));
+
+function writeSomething(message,...args)
+{
+    
+    return  message + args[1]+", "+args[0];
+    
+    
+}
+
+$('#destrucParam1').html(destructParam('Message',{'last':'Smith','first':'John'}));
+    
+function destructParam(msg,{last:lastName,first:firstName})
+{
+   return msg + " ho ho "+lastName+", "+firstName;
+}
+
+/////////////////////////////////////////////////
+
+let numbers = [10,20,30,40,50];
+ 
+$('#destrucArray1').append(destructArray(numbers));
+
+function destructArray([first,second,...remainder])
+{
+    let val = "<ol>";
+    val += "<li>"+first+'</li>'
+    val += "<li>"+second+'</li>';
+    
+    for (let item of remainder)
+    {
+         val += "<li style='color:blue'>"+item+' from for loop</li>';
+    }
+    
+    remainder.forEach((item)=>{
+        val += "<li style='color:red'>"+item+'</li>';
+        
+    })
+    
+    
+    val += "<li>rest has length of "+remainder.length+'</li>'
+    
+    val = val +"</ol>"
+    return val;
+}
+
+let sample = {'last':'Smith','first':'John'};
+$('#templateItem').append(templateDemo(sample));
+
+
+
+function templateDemo(thing)
+{
+    //requires backtick to get the interpolation
+    let templateString = `<h3>This is for ${thing.first} ${thing.last}</h3>`;
+    return templateString;
+}
+
+
